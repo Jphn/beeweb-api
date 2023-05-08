e57cc0434b95e57acb0fc3f7377d55801f041db1
Jphn <joaopedroholandaneves@gmail.com>
2023-05-08 13:33:50 -0300

    feat(valueObjects): create password class

---
+++ src/app/value-objects/password.ts
@@ -0,0 +1,35 @@
+import { compare, hash } from 'bcrypt';
+import { randomInt } from 'crypto';
+import { Either, right } from '../errors/either';
+import { ValueObject } from './value-object';
+
+export interface PasswordProps {
+	value: string;
+}
+
+export class Password extends ValueObject<PasswordProps> {
+	private constructor(props: PasswordProps) {
+		super(props);
+	}
+
+	private static async crypto(password: string) {
+		const randomSalt = randomInt(10, 16);
+		const passwordHash = await hash(password, randomSalt);
+
+		return passwordHash;
+	}
+
+	static async create(password: string): Promise<Either<Error, Password>> {
+		password = await Password.crypto(password);
+
+		return right(new Password({ value: password }));
+	}
+
+	async compare(compareTo: string) {
+		return await compare(compareTo, this.props.value);
+	}
+
+	get value() {
+		return this.props.value;
+	}
+}
