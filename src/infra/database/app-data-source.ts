7162401425016a9b277573adb35a2eac53bdcca1
Jphn <joaopedroholandaneves@gmail.com>
2023-05-18 14:07:08 -0300

    feat(database): create app data source

---
+++ src/infra/database/app-data-source.ts
@@ -0,0 +1,21 @@
+import { DataSource } from 'typeorm';
+import environment from '../../environment';
+
+const {
+	TYPEORM_HOST,
+	TYPEORM_PORT,
+	TYPEORM_USERNAME,
+	TYPEORM_PASSWORD,
+	TYPEORM_DATABASE,
+} = environment;
+
+export const AppDataSource = new DataSource({
+	type: 'postgres',
+	host: TYPEORM_HOST,
+	port: TYPEORM_PORT,
+	username: TYPEORM_USERNAME,
+	password: TYPEORM_PASSWORD,
+	database: TYPEORM_DATABASE,
+	migrations: ['src/infra/database/migrations/*.ts'],
+	entities: ['src/infra/database/schemas/*.ts'],
+});
