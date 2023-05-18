9fc040b4f89c2fb996ebc53c75ab9123f8feb74d
Jphn <joaopedroholandaneves@gmail.com>
2023-05-18 14:24:58 -0300

    feat(main): add database initialization with server

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,26 @@
+import 'reflect-metadata';
+import environment from './environment';
 import app from './infra/app';
+import { AppDataSource } from './infra/database/app-data-source';
 
 (async () => {
-	app.listen({ port: 3000 }, function (err, address) {
-		if (err) {
-			console.log(err);
-			process.exit(1);
-		}
+	try {
+		const { PORT } = environment;
 
-		console.log(`listening at ${address}`);
-	});
+		await AppDataSource.initialize();
+
+		console.log('[main.ts] Successfully connected to data source!');
+
+		app.listen({ port: PORT || 3000 }, function (err, address) {
+			if (err) {
+				console.log(err);
+				process.exit(1);
+			}
+
+			console.log(`[main.ts] Listening at ${address}`);
+		});
+	} catch (err) {
+		console.log(err);
+		process.exit(1);
+	}
 })();
