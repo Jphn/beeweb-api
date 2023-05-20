c52ecc4367df6d0cf27f33b54ad091ed85f37f34
Jphn <joaopedroholandaneves@gmail.com>
2023-05-20 12:22:51 -0300

    feat(http/routes): create user sign in first look

---
+++ src/infra/http/routes/user/sing-in.ts
@@ -0,0 +1,44 @@
+import { UserSignIn } from '../../../../app/use-cases/user-sign-in/user-sign-in';
+import { FastifyJsonSchemaToTsInstance } from '../../../app';
+import { postgresUsersRepository } from '../../../database/repositories';
+import userJsonSchema from './schemas/userJsonSchema';
+
+const path = '/auth';
+
+const body = {
+	type: 'object',
+
+	required: ['email', 'password'],
+
+	additionalProperties: false,
+
+	properties: {
+		email: userJsonSchema.properties.email,
+
+		password: userJsonSchema.properties.password,
+	},
+} as const;
+
+export default async function (app: FastifyJsonSchemaToTsInstance) {
+	const usersRepository = postgresUsersRepository;
+	const userSignIn = new UserSignIn(usersRepository);
+
+	app.post(
+		path,
+		{
+			schema: {
+				body,
+			},
+		},
+		async function (request, reply) {
+			const response = await userSignIn.execute({
+				email: request.body.email,
+				password: request.body.password,
+			});
+
+			if (response.isLeft()) return { msg: response.value.message };
+
+			return response.value;
+		}
+	);
+}
