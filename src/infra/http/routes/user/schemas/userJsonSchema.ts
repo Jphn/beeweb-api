0d504d00ea0356a71077cabc714d22e3e654131b
Jphn <joaopedroholandaneves@gmail.com>
2023-05-20 12:21:04 -0300

    feat(http/routes): create user json schema

---
+++ src/infra/http/routes/user/schemas/userJsonSchema.ts
@@ -0,0 +1,25 @@
+export default {
+	type: 'object',
+
+	required: ['firstName', 'lastName', 'email', 'password', 'isAdmin'],
+
+	additionalProperties: false,
+
+	properties: {
+		firstName: { type: 'string', minLength: 4 },
+
+		lastName: { type: 'string', minLength: 4 },
+
+		email: { type: 'string', minLength: 10 },
+
+		password: {
+			type: 'string',
+			minLength: 8,
+			maxLength: 14,
+		},
+
+		isAdmin: {
+			type: 'boolean',
+		},
+	},
+} as const;
