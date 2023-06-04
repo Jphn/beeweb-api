12293e414085d2379022ec0f886a81a0672f4e79
Jphn <joaopedroholandaneves@gmail.com>
2023-06-04 14:56:09 -0300

    feat(http/routes): create get user infos

---
+++ src/infra/http/routes/user/get-info.ts
@@ -0,0 +1,46 @@
+import { CustomError } from '../../../../app/errors/custom-error';
+import { GetUserInfoUseCase } from '../../../../app/use-cases/get-user-info/get-user-info';
+import { FastifyJsonSchemaToTsInstance } from '../../../app';
+import { postgresUsersRepository } from '../../../database/repositories';
+
+const path = '/:id';
+
+const params = {
+	type: 'object',
+
+	required: ['id'],
+
+	additionalProperties: false,
+
+	properties: {
+		id: { type: 'string', format: 'uuid' },
+	},
+} as const;
+
+export default async function (app: FastifyJsonSchemaToTsInstance) {
+	const usersRepository = postgresUsersRepository;
+	const getUserInfoUseCase = new GetUserInfoUseCase(usersRepository);
+
+	app.get(
+		path,
+		{
+			schema: {
+				params,
+			},
+		},
+		async function (req, res) {
+			await req.jwtVerify();
+
+			if (!req.user.isAdmin)
+				return new CustomError('Not authorized!', 401);
+
+			const response = await getUserInfoUseCase.execute({
+				id: req.params.id,
+			});
+
+			if (response.isLeft()) return response.value;
+
+			return response.value;
+		}
+	);
+}
