fcee9aa8a1a0c18d3dc745300f4736b067f849e5
Jphn <joaopedroholandaneves@gmail.com>
2023-07-11 16:05:41 -0300

    refactor(app/entities): change apiary create method return

--- src/app/entities/apiary.ts
+++ src/app/entities/apiary.ts
@@ -1,3 +1,5 @@
+import { CustomError } from '../errors/custom-error';
+import { Either, right } from '../errors/either';
 import { Entity } from './entity';
 
 export interface ApiaryProps {
@@ -9,7 +11,7 @@ export class Apiary extends Entity<ApiaryProps> {
 		super(props, _id);
 	}
 
-	static create({ name }: ApiaryProps, _id?: string) {
-		return new Apiary({ name }, _id);
+	static create({ name }: ApiaryProps, _id?: string): Either<CustomError, Apiary> {
+		return right(new Apiary({ name }, _id));
 	}
 }
